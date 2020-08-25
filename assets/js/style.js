e9862d5f1b4ebfe63ea0c3ea03be6072ae69c20e
amanduke <65379991+amanduke@users.noreply.github.com>
2020-08-25 16:57:08 -0400

    callback function

--- assets/js/style.js
+++ assets/js/style.js
@@ -1,2 +1,26 @@
+var buttonEl = document.querySelector("#save-task");
+var tasksToDoEl = document.querySelector("#tasks-to-do");
+
+
+var createTaskHandler = function() {
+    var listItemEl = document.createElement("li");
+    listItemEl.className = "task-item";
+    listItemEl.textContent = "This is a new task.";
+    tasksToDoEl.appendChilld(listItemEl);
+}
+
+// not sure why TypeError gets caught, the button still generates a new task though
+buttonEl.addEventListener("click", createTaskHandler);
+
+buttonEl.addEventListener("click", function() {
+    var listItemEl = document.createElement("li");
+    listItemEl.className = "task-item";
+    listItemEl.textContent = "This is a new task.";
+    tasksToDoEl.appendChild(listItemEl);
+});
+
+
 console.log(window.document);
-console.dir(window.document);
+var buttonEl = document.querySelector("#save-task");
+console.log(buttonEl);
+
