0d55ed31b0588f13c10e3f03ce7a5a19462ee506
amanduke <65379991+amanduke@users.noreply.github.com>
2020-08-26 10:22:59 -0400

    updated task handler

--- assets/js/style.js
+++ assets/js/style.js
@@ -2,21 +2,38 @@ var formEl = document.querySelector("#task-form");
 var tasksToDoEl = document.querySelector("#tasks-to-do");
 
 
-var createTaskHandler = function(event) {
-
+var taskFormHandler = function(event) {
     event.preventDefault();
-  
-    var listItemEl = document.createElement("li");
-    listItemEl.className = "task-item";
-    listItemEl.textContent = "This is a new task.";
-    tasksToDoEl.appendChild(listItemEl);
-  };
+    var taskNameInput = document.querySelector("input[name='task-name']").value;
+    var taskTypeInput = document.querySelector("select[name='task-type']").value;
+
+    // package up data as an object
+    var taskDataobj = {
+        name: taskNameInput,
+        type: taskTypeInput
+    };
+
+
+ // create list item
+var listItemEl = document.createElement("li");
+listItemEl.className = "task-item";
+
+// create div to hold task info and add to list item
+var taskInfoEl = document.createElement("div");
+taskInfoEl.className = "task-info";
+taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskNameInput + "</h3><span class='task-type'>" + taskTypeInput + "</span>";
+
+listItemEl.appendChild(taskInfoEl);
+
+// add entire list item to list
+tasksToDoEl.appendChild(listItemEl);
+
+};
+
+
 
 // not sure why TypeError gets caught, the button still generates a new task though
-formEl.addEventListener("submit", createTaskHandler);
-
-
-
+formEl.addEventListener("submit", taskFormHandler);
 
 
 
