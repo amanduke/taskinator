f6357eb9ef867fcab212a8143c2880a3b6d562f5
amanduke <65379991+amanduke@users.noreply.github.com>
2020-08-26 11:35:30 -0400

    develop branch commit

---
+++ assets/js/style.js
@@ -0,0 +1,71 @@
+var formEl = document.querySelector("#task-form");
+var tasksToDoEl = document.querySelector("#tasks-to-do");
+
+
+var taskFormHandler = function(event) {
+    event.preventDefault();
+    var taskNameInput = document.querySelector("input[name='task-name']").value;
+    var taskTypeInput = document.querySelector("select[name='task-type']").value;
+
+    // check if input values are empty strings
+    if (!taskNameInput || !taskTypeInput) {
+        alert("You need to fill out the task form!");
+        return false;
+    }
+
+    formEl.reset();
+
+    if (true) {
+        // this will run because true is true
+          console.log("Is true true? Yes.");
+        }
+        
+        if (false) {
+        // this will not run because false is not true
+          console.log("Is false true? No.");
+        }
+        
+        if (3 === 10 || "a" === "a") {
+        // this will run because at least one of the conditions is true
+          console.log("Does 3 equal 10? No.");
+          console.log("Does the letter 'a' equal the letter 'a'? Yes.");
+        }
+        
+        if (3 === 10 && "a" === "a") {
+        // this will not run because both conditions have to be true to run
+          console.log("Does 3 equal 10? No.");
+          console.log("Does the letter 'a' equal the letter 'a'? Yes.");
+        }
+
+    // package up data as an object
+    var taskDataobj = {
+        name: taskNameInput,
+        type: taskTypeInput
+    };
+
+
+ // create list item
+var listItemEl = document.createElement("li");
+listItemEl.className = "task-item";
+
+// create div to hold task info and add to list item
+var taskInfoEl = document.createElement("div");
+taskInfoEl.className = "task-info";
+taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskNameInput + "</h3><span class='task-type'>" + taskTypeInput + "</span>";
+
+listItemEl.appendChild(taskInfoEl);
+
+// add entire list item to list
+tasksToDoEl.appendChild(listItemEl);
+
+};
+
+
+
+// not sure why TypeError gets caught, the button still generates a new task though
+formEl.addEventListener("submit", taskFormHandler);
+
+
+// add to develop branch
+
+
