df168a95301e6ba8d475ec84445be7600a660e5a
amanduke <65379991+amanduke@users.noreply.github.com>
2020-08-26 11:22:35 -0400

    last update to adding tasks

--- assets/js/style.js
+++ assets/js/style.js
@@ -7,6 +7,36 @@ var taskFormHandler = function(event) {
     var taskNameInput = document.querySelector("input[name='task-name']").value;
     var taskTypeInput = document.querySelector("select[name='task-type']").value;
 
+    // check if input values are empty strings
+    if (!taskNameInput || !taskTypeInput) {
+        alert("You need to fill out the task form!");
+        return false;
+    }
+
+    formEl.reset();
+
+    if (true) {
+        // this will run because true is true
+          console.log("Is true true? Yes.");
+        }
+        
+        if (false) {
+        // this will not run because false is not true
+          console.log("Is false true? No.");
+        }
+        
+        if (3 === 10 || "a" === "a") {
+        // this will run because at least one of the conditions is true
+          console.log("Does 3 equal 10? No.");
+          console.log("Does the letter 'a' equal the letter 'a'? Yes.");
+        }
+        
+        if (3 === 10 && "a" === "a") {
+        // this will not run because both conditions have to be true to run
+          console.log("Does 3 equal 10? No.");
+          console.log("Does the letter 'a' equal the letter 'a'? Yes.");
+        }
+
     // package up data as an object
     var taskDataobj = {
         name: taskNameInput,
